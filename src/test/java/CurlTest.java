ebc4aab92256f90ebcc3c7e181807e2aaebea6a5
ryota100825 <c611701465@tokushima-u.ac.jp>
2019-12-01 15:45:58 +0900

    パラメータ付きPOST機能の追加

--- src/test/java/CurlTest.java
+++ src/test/java/CurlTest.java
@@ -8,16 +8,17 @@ public class CurlTest {
 
     public static void main(String[] args) {
         System.out.println("□ □ □ □ □ 結果 □ □ □ □ □");
-        System.out.println(TrimString(POST()));
+        System.out.println(TrimString(POSTd()));
         //OutputFile(TrimString(Get()));
     }
 
     // Getメソッド
-    public static String Get(){
+    public static String GET(){
         String urlText = "https://example.com"; // 本来はコンソールから読み込む
         HttpURLConnection httpURLConnection = null;
         InputStream inputResultText = null; // 結果（バイト）を格納する変数
         BufferedReader bufferedReader = null; // 結果を読み込み格納する
+        StringBuilder outputResultText = new StringBuilder(); // 結果を格納する
 
         try {
             // try:必ず実行する処理
@@ -31,7 +32,6 @@ public class CurlTest {
             if(httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
                 inputResultText = httpURLConnection.getInputStream(); // ストリームを取得
                 bufferedReader = new BufferedReader(new InputStreamReader(inputResultText)); // ストリームから文字列読み取り
-                StringBuilder outputResultText = new StringBuilder(); // 結果を格納する
                 String resultText; // readLine()の際に格納する
 
                 // ストリングビルダーに最後の文字まで格納する
@@ -42,7 +42,7 @@ public class CurlTest {
             }
         } catch (IOException e){
             // catch:例外処理
-            System.out.println(e.getMessage()); // エラーメッセージを表示
+            e.printStackTrace(); // エラー箇所を表示
         } finally {
             // 例外してもしなくても実行される
             try {
@@ -53,18 +53,19 @@ public class CurlTest {
                     httpURLConnection.disconnect(); // 接続を切る
                 }
             } catch (IOException e){
-                System.out.println(e.getMessage()); // エラーメッセージを表示
+                e.printStackTrace(); // エラー箇所を表示
             }
         }
         return null;
     }
 
     // Get-vメソッド
-    public static String Getv(){
+    public static String GETv(){
         String urlText = "https://example.com"; // 本来はコンソールから読み込む
         HttpURLConnection httpURLConnection = null;
         InputStream inputResultText = null; // 結果（バイト）を格納する変数
         BufferedReader bufferedReader = null; // 結果を読み込み格納する
+        StringBuilder outputResultText = new StringBuilder(); // 結果を格納する
 
         try {
             // try:必ず実行する処理
@@ -79,18 +80,15 @@ public class CurlTest {
             if(httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
                 inputResultText = httpURLConnection.getInputStream(); // ストリームを取得
                 bufferedReader = new BufferedReader(new InputStreamReader(inputResultText)); // ストリームから文字列読み取り
-                StringBuilder outputResultText = new StringBuilder(); // 結果を格納する
                 String resultText; // readLine()の際に格納する
-
                 // ストリングビルダーに最後の文字まで格納する
                 while ((resultText = bufferedReader.readLine()) != null){
                     outputResultText.append(resultText);
                 }
-                return outputResultText.toString();
             }
         } catch (IOException e){
             // catch:例外処理
-            System.out.println(e.getMessage()); // エラーメッセージを表示
+            e.printStackTrace();
         } finally {
             // 例外してもしなくても実行される
             try {
@@ -101,10 +99,10 @@ public class CurlTest {
                     httpURLConnection.disconnect(); // 接続を切る
                 }
             } catch (IOException e){
-                System.out.println(e.getMessage()); // エラーメッセージを表示
+                e.printStackTrace();
             }
         }
-        return null;
+        return outputResultText.toString();
     }
 
     // Postメソッド
@@ -113,20 +111,20 @@ public class CurlTest {
         HttpURLConnection httpURLConnection = null;
         InputStream inputResultText = null; // 結果（バイト）を格納する変数
         BufferedReader bufferedReader = null; // 結果を読み込み格納する
+        StringBuilder outputResultText = new StringBuilder(); // 結果を格納する
 
         try {
             // try:必ず実行する処理
             URL url = new URL(urlText); // 接続するURLを指定しインスタンス生成
             // コネクションを取得（URLが参照するリモート・オブジェクトへの接続を表すインスタンスを取得）
             httpURLConnection = (HttpURLConnection)url.openConnection();
-            httpURLConnection.setRequestMethod("POST"); // GETメソッドをセット
+            httpURLConnection.setRequestMethod("POST"); // POSTメソッドをセット
             httpURLConnection.connect(); // コネクト
 
             // 応答されたコードがHTTP_OK(200)なら結果を読み込む
             if(httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK){
                 inputResultText = httpURLConnection.getInputStream(); // ストリームを取得
                 bufferedReader = new BufferedReader(new InputStreamReader(inputResultText)); // ストリームから文字列読み取り
-                StringBuilder outputResultText = new StringBuilder(); // 結果を格納する
                 String resultText; // readLine()の際に格納する
 
                 // ストリングビルダーに最後の文字まで格納する
@@ -137,7 +135,7 @@ public class CurlTest {
             }
         } catch (IOException e){
             // catch:例外処理
-            System.out.println(e.getMessage()); // エラーメッセージを表示
+            e.printStackTrace();
         } finally {
             // 例外してもしなくても実行される
             try {
@@ -148,12 +146,75 @@ public class CurlTest {
                     httpURLConnection.disconnect(); // 接続を切る
                 }
             } catch (IOException e){
-                System.out.println(e.getMessage()); // エラーメッセージを表示
+                e.printStackTrace();
             }
         }
         return null;
     }
 
+    // POST-dメソッド
+    public static String POSTd(){
+        String urlText = "https://example.com/";
+        String param = "key=value";
+        HttpURLConnection httpURLConnection = null;
+        InputStream inputResultText = null; // 結果（バイト）
+        BufferedReader bufferedReader = null;
+        StringBuilder result = new StringBuilder();
+
+        try {
+            URL url = new URL(urlText); // 接続するURLを指定したインスタンスを生成
+            // コネクションを取得（URLが参照するリモート・オブジェクトへの接続を表すインスタンスを取得）
+            httpURLConnection = (HttpURLConnection)url.openConnection();
+            httpURLConnection.setDoOutput(true); // 出力を可能にする
+            httpURLConnection.setRequestMethod("POST"); // POSTメソッドをセット
+
+            // この接続に出力するストリーム
+            OutputStreamWriter out = new OutputStreamWriter(httpURLConnection.getOutputStream());
+            out.write(param); // 書き込み
+            out.close(); // 書き込み終了
+
+            httpURLConnection.connect(); // コネクト
+
+            // 応答されたコードがHTTP_OK(200)なら結果を読み込む
+            if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                inputResultText = httpURLConnection.getInputStream(); // ストリームを取得
+                bufferedReader = new BufferedReader(new InputStreamReader(inputResultText)); // ストリームを読み取り文字列を読み取る
+                String line; // readLine()の際に格納
+
+                // テキストを読み込み
+                while((line = bufferedReader.readLine()) != null) {
+                    result.append(line);
+                }
+
+            }else { // コードがHTTP_OKじゃないならエラー結果を読み取る
+                // エラー表示を読み込み
+                inputResultText = httpURLConnection.getErrorStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputResultText));
+                String line;
+
+                // テキストを読み込み
+                while((line = bufferedReader.readLine()) != null) {
+                    result.append(line);
+                }
+
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+                if (httpURLConnection != null) {
+                    // コネクションを切断
+                    httpURLConnection.disconnect();
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return result.toString();
+    }
 
     // ヘッダーをプリントするメソッド
     private static void PrintHeader(HttpURLConnection httpURLConnection){
@@ -169,8 +230,7 @@ public class CurlTest {
     // 文字列整形メソッド
     // 改行したいkeyを選択し、改行メソッドを実行
     // 整形した文字列をStringで返す
-    public static String TrimString(String resultText)
-    {
+    public static String TrimString(String resultText) {
         StringBuilder trimedResultText = new StringBuilder(resultText);
 
         InsertNewLine(trimedResultText,'>');
